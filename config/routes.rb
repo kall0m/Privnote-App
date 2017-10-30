edc2ff7d3804bc9d0030b9f2cf0c4f49cd8d7e11
Dani-Ray <daniel_raykov@abv.bg>
2017-10-30 12:21:13 +0200

    Update from class

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
 
   resources :messages
 
-  #post "messages/get_url" => "messages#get_url"
+  post "notes/api" => "messages#create"
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
